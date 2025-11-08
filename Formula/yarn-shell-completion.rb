fbf5485dc6d9789e5047e3487d43a4ce8f530f95
Mina Demian <mina@minademian.com>
2025-11-08 20:32:06 +0100

    fix: wrong url

--- Formula/yarn-shell-completion.rb
+++ Formula/yarn-shell-completion.rb
@@ -2,7 +2,7 @@
 class YarnShellCompletion < Formula
   desc "Bash and Zsh completion for Yarn 4+"
   homepage "https://github.com/ursine-code/yarn-shell-completion"
-  url "https://github.com/ursine-code/yarn-shell-completion/archive/v1.0.0.tar.gz"
+  url "https://github.com/ursine-code/yarn-shell-completion/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0d13ceb6ebeb165032c930581261a07acf9ae368b0140cea5077e44ec54c5591"
   license "MIT"
   
