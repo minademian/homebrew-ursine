2c162d2ad5ee2ef53ee835cdedb91e020f51f403
Mina Demian <mina@minademian.com>
2025-11-08 20:12:22 +0100

    chore: final formula

--- Formula/yarn-completion.rb
+++ Formula/yarn-completion.rb
@@ -3,7 +3,7 @@ class YarnCompletion < Formula
   desc "Bash and Zsh completion for Yarn 4+"
   homepage "https://github.com/ursine-code/yarn-shell-completion"
   url "https://github.com/ursine-code/yarn-shell-completion/archive/v1.0.0.tar.gz"
-  sha256 "d725f54939e76bc8084425df1514a975bc5e981b84cf4c91df8f4234676b465"
+  sha256 "0d13ceb6ebeb165032c930581261a07acf9ae368b0140cea5077e44ec54c5591"
   license "MIT"
   
   def install
@@ -13,5 +13,6 @@ class YarnCompletion < Formula
   
   test do
     assert_match "yarn", shell_output("bash -c 'source #{bash_completion}/yarn'")
+    assert_match "_yarn", shell_output("zsh -c 'source #{zsh_completion}/_yarn'")
   end
 end
