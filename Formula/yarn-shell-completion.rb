d84e4df380ee645ae899eff4bbed8c738c84c209
Mina Demian <mina@minademian.com>
2025-11-09 11:54:07 +0100

    chore: update Formula to v1.0.6 release

--- Formula/yarn-shell-completion.rb
+++ Formula/yarn-shell-completion.rb
@@ -2,8 +2,8 @@
 class YarnShellCompletion < Formula
   desc "Bash and Zsh completion for Yarn 4+"
   homepage "https://github.com/ursine-code/yarn-shell-completion"
-  url "https://github.com/ursine-code/yarn-shell-completion/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "17ed8c85c55fc362dbf5f0930347004f9a18645a9bdc342f1e4520855da6e858"
+  url "https://github.com/ursine-code/yarn-shell-completion/archive/refs/tags/v1.0.6.tar.gz"
+  sha256 "f8f18c6b704bdf8be744cf11e02e264b5484c81c9dd3da423345748cfe54fd6b"
   license "MIT"
   
   def install
@@ -12,7 +12,7 @@ class YarnShellCompletion < Formula
   end
   
   test do
-    assert_match "yarn", shell_output("bash -c 'source #{bash_completion}/yarn'")
-    assert_match "_yarn", shell_output("zsh -c 'source #{zsh_completion}/_yarn'")
+    system "bash", "-c", "source #{bash_completion}/yarn"
+    system "zsh", "-c", "autoload -U +X bashcompinit && bashcompinit && source #{zsh_completion}/_yarn"
   end
 end
