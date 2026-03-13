b03ba5f1dcb3d17fe01701775f532b806c8ff453
Mina Demian <mina@minademian.com>
2026-03-13 03:22:08 +0100

    chore: update Formula to v1.0.8 release

--- Formula/yarn-shell-completion.rb
+++ Formula/yarn-shell-completion.rb
@@ -5,7 +5,8 @@ class YarnShellCompletion < Formula
   url "https://github.com/ursine-code/yarn-shell-completion/archive/refs/tags/v1.0.7.tar.gz"
   sha256 "9e0e5422394071cec29ed257ce3da49f1f7028a6ed6f55f24a1aafd5b5784048"
   license "MIT"
-  
+  bottle :unneeded
+
   def install
     bash_completion.install "completions/yarn" => "yarn"
     zsh_completion.install "completions/_yarn" => "_yarn"
